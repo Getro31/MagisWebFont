990172d5a8e965d9e6dcdc59b95fb4654a4f89b7
Semushahu <semushahuerv@gmail.com>
2024-11-19 17:03:37 +0200

    clean and build scripts

---
+++ figma.config.ts
@@ -0,0 +1,41 @@
+// @ts-check
+import outputComponentsAsSvg from "@figma-export/output-components-as-svg";
+import transformSvgWithSvgo from "@figma-export/transform-svg-with-svgo";
+import dotenv from "dotenv";
+
+dotenv.config();
+const fileId = process.env.FILE_ID;
+
+/** @type { import('@figma-export/types').ComponentsCommandOptions } */
+const componentOptions = {
+  fileId,
+  onlyFromPages: ["solid"],
+  transformers: [
+    transformSvgWithSvgo({
+      multipass: true,
+      plugins: [
+        {
+          name: "preset-default",
+          params: {
+            overrides: {
+              removeViewBox: false,
+            },
+          },
+        },
+        {
+          name: "removeDimensions",
+        },
+      ],
+    }),
+  ],
+  outputters: [
+    outputComponentsAsSvg({
+      output: "./",
+    }),
+  ],
+};
+
+/** @type { import('@figma-export/types').FigmaExportRC } */
+export default {
+  commands: [["components", componentOptions]],
+};
