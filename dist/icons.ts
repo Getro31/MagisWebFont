9bfa8a555693de221ccc8872510815aeaaf7a82e
Semushahu <semushahuerv@gmail.com>
2024-11-19 18:08:41 +0200

    update icons

---
+++ dist/icons.ts
@@ -0,0 +1,13 @@
+export type IconsId =
+  | "Bourse";
+
+export type IconsKey =
+  | "Bourse";
+
+export enum Icons {
+  Bourse = "Bourse",
+}
+
+export const ICONS_CODEPOINTS: { [key in Icons]: string } = {
+  [Icons.Bourse]: "61697",
+};
---
+++ icons.js
@@ -0,0 +1,1 @@
+export { default as magisFont } from "./dist/magis.svg";
